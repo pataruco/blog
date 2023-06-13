e9e9c3f26fdcf2cbcbc703e384e1d89db73f913d
Pedro Martin <pataruco@gmail.com>
2023-06-13 16:15:43 +0100

    Frontmatter working

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
 import nextMDX from '@next/mdx';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkGfm from 'remark-gfm';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+    mdxRs: true,
   },
 };
 
 const withMDX = nextMDX({
   options: {
-    remarkPlugins: [],
+    remarkPlugins: [
+      remarkGfm,
+      [remarkFrontmatter, { type: 'yaml', marker: '-' }],
+    ],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
@@ -17,5 +23,3 @@ const withMDX = nextMDX({
 });
 
 export default withMDX(nextConfig);
-
-// module.exports = withMDX(nextConfig);
--- src/app/blog/a11y-is-your-ally/page.tsx
+++ src/app/blog/a11y-is-your-ally/page.tsx
@@ -1,5 +1,8 @@
 'use client';
-import A11yIsYourAlly from './post.mdx';
+// @ts-expect-error
+import A11yIsYourAlly, { meta } from './post.mdx';
+
+console.log({ meta });
 
 export default function Page() {
   return <A11yIsYourAlly />;
