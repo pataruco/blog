98eaddba349613eb8ed8496c88d825f5ad9e6f6c
Pedro Martin <pataruco@gmail.com>
2023-06-13 16:48:01 +0100

    Add frontmatter

--- src/app/blog/a11y-is-your-ally/page.tsx
+++ src/app/blog/a11y-is-your-ally/page.tsx
@@ -1,8 +1,9 @@
 'use client';
+import { PostMeta } from '@/types';
 // @ts-expect-error
 import A11yIsYourAlly, { meta } from './post.mdx';
 
-console.log({ meta });
+export const { title, excerpt, date }: PostMeta = meta;
 
 export default function Page() {
   return <A11yIsYourAlly />;
