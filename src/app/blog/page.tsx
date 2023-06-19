bbdf40bec23724efa6ca8829a16465dac8c6c4ec
Pedro Martin <pataruco@gmail.com>
2023-06-19 15:10:15 +0100

    Add microdata on blog index

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,9 +3,29 @@
 import { PostItem } from '@/components/post-item';
 import { postsInfo } from './all-info';
 
+import { BlogPosting, WithContext } from 'schema-dts';
+
+const jsonLd: WithContext<BlogPosting> = {
+  '@context': 'https://schema.org',
+  '@type': 'BlogPosting',
+  name: 'Blog',
+  description:
+    'My thoughts about building, teaching and leading product tech teams',
+  url: 'https://pataruco.dev/blog',
+  author: {
+    '@type': 'Person',
+    name: 'Pedro Martin Valera',
+  },
+};
+
 const Blog = () => {
   return (
     <>
+      <script
+        type="application/ld+json"
+        // rome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <div className="content">
         <h1>Blog</h1>
         <p>
