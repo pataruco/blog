000284dbb084f747d27cf77a06df18918147ea17
Pedro Martin <pataruco@gmail.com>
2023-06-19 11:00:08 +0100

    Set post to be a component

--- src/app/blog/how-we-might-influence-product-decision-makers-about-accessibility-in-digital-products/page.tsx
+++ src/app/blog/how-we-might-influence-product-decision-makers-about-accessibility-in-digital-products/page.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+import meta from './info';
 import Content from './post.mdx';
 
+const { date, title } = meta;
+
 export default function Page() {
-  return <Content />;
+  return (
+    <>
+      <h1>{title}</h1>
+      <div className="meta">
+        <time dateTime={date.toISOString()}>
+          {new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(date)}
+        </time>
+      </div>
+      <Content />
+    </>
+  );
 }
