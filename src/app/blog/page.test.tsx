d72a893cc6141c8db68c098e426d570f20245df6
Pedro Martin <pataruco@gmail.com>
2023-06-21 14:33:32 +0100

    Test blog index

---
+++ src/app/blog/page.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import { Blog, WithContext } from 'schema-dts';
+import { describe, expect, it } from 'vitest';
+
+import { about, author } from '@/utils/metadata';
+import BlogComponent from './page';
+
+describe('Blog index', () => {
+  it('renders', () => {
+    const jsonLd: WithContext<Blog> = {
+      '@context': 'https://schema.org',
+      '@type': 'Blog',
+      about,
+      author,
+    };
+
+    const { container } = render(<BlogComponent />);
+
+    expect(container.querySelector('h1')?.textContent).toBe('Blog');
+    expect(container.querySelector('p')?.textContent).toBe(
+      'My thoughts about building, teaching and leading product tech teams',
+    );
+    expect(container.querySelector('script')?.textContent).toBe(
+      JSON.stringify(jsonLd),
+    );
+  });
+});
