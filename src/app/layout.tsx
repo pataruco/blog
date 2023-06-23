5c6c4c6c5fe64ef0a607bf5a58ef394f3644b217
Pedro Martin <pataruco@gmail.com>
2023-06-23 10:09:52 +0100

    Draft og images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,31 @@
 import Footer from '@/components/footer';
 import Header from '@/components/header';
 import '@/styles/index.css';
+import { BASE_URL } from '@/utils/metadata';
 import { Analytics } from '@vercel/analytics/react';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 export const metadata: Metadata = {
+  metadataBase: new URL(BASE_URL),
   title: 'Pedro Martin Valera',
   description: 'I help to build, teach and lead product tech teams.',
+  openGraph: {
+    title: 'Pedro Martin Valera',
+    description: 'I help to build, teach and lead product tech teams.',
+    url: BASE_URL,
+    siteName: 'Pedro Martin Valera',
+    images: [
+      {
+        url: `${BASE_URL}/og.png`,
+        width: 800,
+        height: 600,
+        alt: 'Pedro Martin Valera',
+      },
+    ],
+    locale: 'en_GB',
+    type: 'website',
+  },
 };
 
 const inter = Inter({
---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,5 @@
+import OgTwitterImage from '@/components/og-twitter-image';
+// Image generation
+export default async function () {
+  return OgTwitterImage();
+}
