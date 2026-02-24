c45b9685401f1b137dddcf33df96270375a86501
pataruco <10330222+pataruco@users.noreply.github.com>
2026-02-24 14:04:27 +0000

    feat: migrate Next.js to vanilla JS Web Components SSG

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
-import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tsconfigPaths()],
   test: {
     environment: 'jsdom',
+    exclude: ['**/node_modules/**', '**/dist/**', '**/tests/**'],
   },
 });
